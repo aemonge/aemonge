0c25fa6db31de599c39069168d1be1d307f5341c
aemonge <andres@aemonge.com>
2023-05-08 11:20:42 +0200

    feat(webcam-fix): Script to fix my webcam

--- env-configs/pacman.sh
+++ env-configs/pacman.sh
@@ -12,11 +12,14 @@ _pacman() {
   sudo pacman-key --populate
   sudo pacman -S --needed base-devel git
   sudo pacman -S linux-neptune-headers
-  sudo pacman -S base-devel linux-headers gcc glibc gcc-libs
+  sudo pacman -S base-devel linux-headers gcc glibc gcc-libs \
+    linux-api-headers
   sudo pacman -S python-pip zsh git tig \
    flameshot glow xclip onboard \
    nodejs npm the_silver_searcher neovim \
    entr clamav
+  # Super extra but useful
+  sudo pacman -S libunistring pcre2
 }
 
 _pikaur() {
@@ -25,10 +28,10 @@ _pikaur() {
    cd /tmp/pikaur
    makepkg -fsri
 
-   pikaur -S --noconfirm linux-steamos
+   pikaur -S --noconfirm linux-steamos libxcrypt-compat
    pikaur -S --noconfirm fzf firefox-pwa-bin konsave firefox-developer-edition \
      telegram-desktop
-   pikaur -S --noconfirm spotify grip
+   pikaur -S --noconfirm spotify grip boxes
 }
 
 # steamos-readonly disable
