0193e96354bd2343828465d0906298a9bc3db1ed
aemonge <andres@aemonge.com>
2023-08-23 19:09:52 +0200

    lint(bash): Linted some scripts

--- reformat-scripts/install.sh
+++ reformat-scripts/install.sh
@@ -1,75 +1,54 @@
 #!/bin/bash
 
-
 system() {
-  set -e
+    set -e
 
-  rm -rf /etc/sddm.conf.d/zz-steamos-autologin.conf
-  ln -s ~/usr/env-configs/zz-steamos-autologin.conf /etc/sddm.conf.d/zz-steamos-autologin.conf
-  ./env-configs/pacman.sh
-}
-
-fonts() {
-  ./env-configs/font.sh
+    rm -rf /etc/sddm.conf.d/zz-steamos-autologin.conf
+    ln -s ~/usr/env-configs/zz-steamos-autologin.conf /etc/sddm.conf.d/zz-steamos-autologin.conf
+    ./pacman.sh
 }
 
 kde() {
-  konsave -i ~/usr/reformat-scripts/deck.knsv
-  konsave -a deck
+    konsave -i ~/usr/reformat-scripts/deck.knsv
+    konsave -a deck
 }
 
-
 configs() {
-  # Applications
-  P="/home/deck/usr/configs/applications/"
-  for f in $(ls -a $P); do
-    if [[ -f "$P/$f"  ]]; then
-      rm ~/$f 2> /dev/null;
-      ln -s $P/$f ~/$f;
-    fi;
-  done;
+    # Applications, Bash, Devtools
+    P="/home/deck/usr/configs/applications/"
 
-  # Bash
-  P="/home/deck/usr/configs/bash/"
-  for f in $(ls -a $P); do
-    if [[ -f "$P/$f"  ]]; then
-      rm ~/$f 2> /dev/null;
-      ln -s $P/$f ~/$f;
-    fi;
-  done;
-
-  # Devtools
-  P="/home/deck/usr/configs/dev-tools/"
-  for f in $(ls -a $P); do
-    if [[ -f "$P/$f"  ]]; then
-      rm ~/$f 2> /dev/null;
-      ln -s $P/$f ~/$f;
-    fi;
-  done;
+    for P in "/home/deck/usr/configs/applications/" "/home/deck/usr/configs/bash/" "/home/deck/usr/configs/dev-tools/"; do
+        for f in $(ls -a $P); do
+            if [[ -f "$P/$f" ]]; then
+                rm "$HOME/$f" 2>/dev/null
+                ln -s "$P/$f" "$HOME/$f"
+            fi
+        done
+    done
 }
 
 firefox() {
-  cd /home/deck/.mozilla/firefox/40f5zrsq.deck*
-  mkdir -p chrome 2> /dev/null
-  cd chrome
-  rm userChrome.css 2> /dev/null
-  ln -s $HOME/usr/app-configs/userChrome_customization.css userChrome.css
+    cd /home/deck/.mozilla/firefox/40f5zrsq.deck*
+    mkdir -p chrome 2>/dev/null
+    cd chrome
+    rm userChrome.css 2>/dev/null
+    ln -s "$HOME/usr/configs/applications/firefox/chrome/userChrome.css" userChrome.css
 }
 
 nvim() {
-  # pip install neovim-remote
-  mkdir -p ~/.config/nvim/spell
+    # pip install neovim-remote
+    mkdir -p ~/.config/nvim/spell
 
-  rm -rf ~/.config/nvim 2> /dev/null
-  rm -rf ~/.cache/nvim 2> /dev/null
-  rm -rf ~/.local/share/nvim 2> /dev/null
-  rm -rf ~/.local/state/nvim 2> /dev/null
+    rm -rf ~/.config/nvim 2>/dev/null
+    rm -rf ~/.cache/nvim 2>/dev/null
+    rm -rf ~/.local/share/nvim 2>/dev/null
+    rm -rf ~/.local/state/nvim 2>/dev/null
 
-  ln -s ~/usr/nvim ~/.config/.
+    ln -s ~/usr/nvim ~/.config/.
 }
 
 # system
-# fonts
+# ./font.sh
 configs
 nvim
 # kde
--- reformat-scripts/pacman.sh
+++ reformat-scripts/pacman.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 
+set -e
+
 _rwfux() {
-  git clone https://github.com/ValShaped/rwfus.git /tmp/rwfux
-  cd /tmp/rwfux
-  sudo ./rwfus -iI
-  # sudo ./rwfus -e
+    git clone https://github.com/ValShaped/rwfus.git /tmp/rwfus
+    cd /tmp/rwfus
+    sudo ./rwfus -iI
 }
 
 _aura() {
-  git clone https://aur.archlinux.org/aura-bin.git /tmp/aura-bin
-  cd /tmp/aura-bin
-  makepkg
-  sudo pacman -U *pkg.tar.zst
+    git clone https://aur.archlinux.org/aura-bin.git /tmp/aura-bin
+    cd /tmp/aura-bin
+    makepkg
+    sudo pacman -U *pkg.tar.zst
 }
 
 _system() {
-  # Headers to build software like aura and other AURs
-  sudo pacman -S --needed --noconfirm base-devel gcc glibc gcc-libs openssl
+    # Headers to build software like aura and other AURs
+    sudo pacman -S --needed --noconfirm base-devel gcc glibc gcc-libs openssl
 }
 
 _dev_extra() {
-  sudo pacman -S --needed --noconfirm firefox-developer-edition python-pip zsh xclip
-  sudo pacman -S --needed --noconfirm the_silver_searcher entr neovim tig flameshot
-  sudo pacman -S --needed --noconfirm glow onboard nodejs npm docker fzf rustup
-  sudo npm i -g npm yarn
+    sudo pacman -S --needed --noconfirm firefox-developer-edition python-pip zsh xclip
+    sudo pacman -S --needed --noconfirm the_silver_searcher entr neovim tig flameshot
+    sudo pacman -S --needed --noconfirm glow onboard nodejs npm docker fzf rustup
+    sudo npm i -g npm yarn
 
-  sudo aura -A --noconfirm --needed firefox-pwa konsave neovim-remote
+    sudo aura -A --noconfirm --needed firefox-pwa konsave neovim-remote
 }
 
 _user_extra() {
-  sudo aura -A --noconfirm --needed kde-servicemenus-clamtkscan clamav-desktop-bin
+    sudo aura -A --noconfirm --needed kde-servicemenus-clamtkscan clamav-desktop-bin
 
-  # User focused
-  sudo aura -A --noconfirm --needed telegram-desktop-bin
-  sudo aura -A --noconfirm --needed --skippgpcheck spotify
+    # User focused
+    sudo aura -A --noconfirm --needed telegram-desktop-bin
+    sudo aura -A --noconfirm --needed --skippgpcheck spotify
 
-  # Work focused
-  sudo aura -A --noconfirm --needed slack-desktop
+    # Work focused
+    sudo aura -A --noconfirm --needed slack-desktop
 }
 
 _rwfux
@@ -44,5 +45,3 @@ _system
 _aura
 _dev_extra
 _user_extra
-
-sudo rwfus -d
