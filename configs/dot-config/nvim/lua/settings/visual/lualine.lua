843c138ea87fddade8f60953d43b5ccc258e484d
aemonge <andres@aemonge.com>
2023-11-24 19:07:31 +0100

    feat(nvim): Lualine show inactive pale color.

--- configs/dot-config/nvim/lua/settings/visual/lualine.lua
+++ configs/dot-config/nvim/lua/settings/visual/lualine.lua
@@ -1,200 +1,211 @@
 local function breadcrumbs()
-    local items = vim.b.coc_nav
-    local t = {}
-    for k, v in ipairs(items) do
-        t[#t + 1] = "" .. (type(v.label) == "string" and v.label .. "" or "")
-            .. (v.name or "")
-        if next(items, k) ~= nil then
-            t[#t + 1] = "  "
-        end
+  local items = vim.b.coc_nav
+  local t = {}
+  for k, v in ipairs(items) do
+    t[#t + 1] = "" .. (type(v.label) == "string" and v.label .. "" or "")
+        .. (v.name or "")
+    if next(items, k) ~= nil then
+      t[#t + 1] = " "
     end
-    return table.concat(t)
+  end
+  return table.concat(t)
 end
 
 local function search_count()
-    if vim.api.nvim_get_vvar("hlsearch") == 1 then
-        local search_expr = vim.fn.getreg("/")
-        local res = vim.fn.searchcount({ maxcount = 999, timeout = 500 })
+  if vim.api.nvim_get_vvar("hlsearch") == 1 then
+    local search_expr = vim.fn.getreg("/")
+    local res = vim.fn.searchcount({ maxcount = 999, timeout = 500 })
 
-        if res.total > 0 then
-            return string.format("/%s/ %d/%d", search_expr, res.current, res.total)
-        end
+    if res.total > 0 then
+      return string.format("/%s/ %d/%d", search_expr, res.current, res.total)
     end
-    return ""
+  end
+  return ""
 end
 
 local function get_mode_symbol()
-    local mode = vim.api.nvim_get_mode().mode
-    local mode_symbol = {
-        ["n"] = { symbol = "" },
-        ["i"] = { symbol = "" },
-        ["r"] = { symbol = "" },
-        ["v"] = { symbol = "" },
-        ["V"] = { symbol = "" },
-        ["s"] = { symbol = "" },
-        ["c"] = { symbol = "" },
-        ["t"] = { symbol = "" },
-        ["R"] = { symbol = "" },
-        ["!"] = { symbol = "" },
-    }
+  local mode = vim.api.nvim_get_mode().mode
+  local mode_symbol = {
+    ["n"] = { symbol = "" },
+    ["i"] = { symbol = "" },
+    ["r"] = { symbol = "" },
+    ["v"] = { symbol = "" },
+    ["V"] = { symbol = "" },
+    ["s"] = { symbol = "" },
+    ["c"] = { symbol = "" },
+    ["t"] = { symbol = "" },
+    ["R"] = { symbol = "" },
+    ["!"] = { symbol = "" },
+  }
 
-    return mode_symbol[mode].symbol or ""
+  return mode_symbol[mode].symbol or ""
 end
 
 
 local diagnostics = {
-    "diagnostics",
-    sources = { "nvim_lsp", "nvim_diagnostic", "coc" },
-    sections = { "error", "warn", "info", "hint" },
-    symbols = { warn = " ", error = " ", info = " ", hint = "𥉉" },
-    colored = true,
-    update_in_insert = true,
-    always_visible = false,
-    subject = function(data)
-        local result = ""
-        for name, count in pairs(data) do
-            if count ~= 0 then
-                result = result .. (diagnostics.symbols[name] or name) .. count .. " "
-            end
-        end
-        return result
+  "diagnostics",
+  sources = { "nvim_lsp", "nvim_diagnostic", "coc" },
+  sections = { "error", "warn", "info", "hint" },
+  symbols = { warn = " ", error = " ", info = " ", hint = "𥉉" },
+  colored = true,
+  update_in_insert = true,
+  always_visible = false,
+  subject = function(data)
+    local result = ""
+    for name, count in pairs(data) do
+      if count ~= 0 then
+        result = result .. (diagnostics.symbols[name] or name) .. count .. " "
+      end
     end
+    return result
+  end
 }
 
 local diff = {
-    "diff",
-    draw_empty = true,
-    colored = true,
-    symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+  "diff",
+  draw_empty = true,
+  colored = true,
+  symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
 }
 
 local branch = {
-    "branch",
-    color = { fg = "#CCCECF", bg = "#4f596e" },
-    icons_enabled = true,
-    draw_empty = true,
-    icon = "",
+  "branch",
+  color = { fg = "#CCCECF", bg = "#4f596e" },
+  icons_enabled = true,
+  draw_empty = true,
+  icon = "",
 }
 
 local M = {
-    "nvim-lualine/lualine.nvim",
-    after = "noice.nvim",
-    config = function()
-        local disabled_filetypes, disabled_buftypes = require("settings.visual.raw-types")
-        local material_theme = require("lualine.themes.material")
-        local winbar_bg = { bg = "#291E15" }
-        local theme = {
-            insert = {
-                a = { fg = "#7E98BA", bg = "#2E3C43" },
-                z = material_theme.normal.z
-            },
-            visual = {
-                a = { bg = "#628B4A", fg = "#2E3C43" },
-                z = material_theme.normal.z
-            },
-            replace = {
-                a = { bg = "#D16969", fg = "#2E3C43" },
-                z = material_theme.normal.z
-            },
-            command = {
-                a = { bg = "#E5C07B", fg = "#2E3C43" },
-                z = material_theme.normal.z
-            },
-            normal = {
-                a = { fg = "#7E98BA", bg = "#2E3C43" },
-                b = { fg = "#7E98BA", bg = "#3B4252" },
-                c = { fg = "#CCCECF", bg = "#4F596E" },
-                x = { fg = "#CCCECF", bg = "#4F596E" },
-                y = { fg = "#CCCECF", bg = "#4F596E" },
-                z = material_theme.normal.z
-            },
-            inactive_winbar = {
-                a = winbar_bg,
-                b = winbar_bg,
-                c = winbar_bg,
-                x = winbar_bg,
-                z = winbar_bg,
-            },
-            winbar = {
-                a = winbar_bg,
-                b = winbar_bg,
-                c = winbar_bg,
-                x = winbar_bg,
-                z = winbar_bg,
-            },
-            inactive = {
-                a = { fg = "#CCCECF", bg = "none" },
-                c = { fg = "#CCCECF", bg = "none" },
-                y = { fg = "#CCCECF", bg = "none" },
-            }
-        }
+  "nvim-lualine/lualine.nvim",
+  after = "noice.nvim",
+  config = function()
+    local disabled_filetypes, disabled_buftypes = require("settings.visual.raw-types")
+    local material_theme = require("lualine.themes.material")
+    local winbar_bg = { bg = "#291E15" }
+    local theme = {
+      insert = {
+        a = { fg = "#7E98BA", bg = "#2E3C43" },
+        z = material_theme.normal.z
+      },
+      visual = {
+        a = { bg = "#628B4A", fg = "#2E3C43" },
+        z = material_theme.normal.z
+      },
+      replace = {
+        a = { bg = "#D16969", fg = "#2E3C43" },
+        z = material_theme.normal.z
+      },
+      command = {
+        a = { bg = "#E5C07B", fg = "#2E3C43" },
+        z = material_theme.normal.z
+      },
+      normal = {
+        a = { fg = "#7E98BA", bg = "#2E3C43" },
+        b = { fg = "#7E98BA", bg = "#3B4252" },
+        c = { fg = "#CCCECF", bg = "#4F596E" },
+        x = { fg = "#CCCECF", bg = "#4F596E" },
+        y = { fg = "#CCCECF", bg = "#4F596E" },
+        z = material_theme.normal.z
+      },
+      inactive_winbar = {
+        a = winbar_bg,
+        b = winbar_bg,
+        c = winbar_bg,
+        x = winbar_bg,
+        z = winbar_bg,
+      },
+      winbar = {
+        a = winbar_bg,
+        b = winbar_bg,
+        c = winbar_bg,
+        x = winbar_bg,
+        z = winbar_bg,
+      },
+      inactive = {
+        a = { fg = "#CCCECF", bg = "none" },
+        c = { fg = "#CCCECF", bg = "none" },
+        y = { fg = "#CCCECF", bg = "none" },
+      }
+    }
 
-        require("lualine").setup({
-            options = {
-                icons_enabled = true,
-                theme = theme,
-                always_visible = true,
-                component_separators = { left = "", right = "" },
-                section_separators = { left = "", right = "" },
-                disabled_filetypes = disabled_filetypes,
-                disabled_buftypes = disabled_buftypes,
+    require("lualine").setup({
+      options = {
+        icons_enabled = true,
+        theme = theme,
+        always_visible = true,
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = disabled_filetypes,
+        disabled_buftypes = disabled_buftypes,
+      },
+      sections = {
+        lualine_a = { diff },
+        lualine_b = {},
+        lualine_c = {
+          { "nvim-tree" },
+          { "filename", path = 1 },
+        },
+        lualine_x = {
+          { search_count, type = "lua_expr" }
+        },
+        lualine_y = {
+          {
+            "g:coc_status",
+            "bo:filetype",
+          },
+        },
+        lualine_z = {
+          {
+            "swenv",
+            cond = function()
+              return vim.bo.filetype == "python"
+            end,
+            icon = "",
+            color = { fg = "#3B4252", bg = "#7E98BA" }
+          }
+        }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = {},
+        lualine_y = {
+          "filetype"
+        },
+        lualine_z = {},
+      },
+      inactive_winbar = {
+        lualine_a = {},
+        lualine_b = {
+          {
+            "branch",
+            color = {
+              bg = "#152528"
             },
-            sections = {
-                lualine_a = {},
-                lualine_b = { diff },
-                lualine_c = {
-                    { "nvim-tree" },
-                    { "filename", path = 1 },
-                },
-                lualine_x = {},
-                lualine_y = {
-                    {
-                        "g:coc_status",
-                        "bo:filetype",
-                    },
-                },
-                lualine_z = {
-                    {
-                        "swenv",
-                        cond = function()
-                            return vim.bo.filetype == "python"
-                        end,
-                        icon = "",
-                        color = { fg = "#3B4252", bg = "#7E98BA" }
-                    }
-                }
-            },
-            inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = { { "filename", path = 1 } },
-                lualine_x = {},
-                lualine_y = {
-                    "filetype"
-                },
-                lualine_z = {},
-            },
-            inactive_winbar = {
-                lualine_a = { get_mode_symbol },
-                lualine_b = { branch },
-                lualine_z = { diagnostics },
-            },
-            winbar = {
-                lualine_a = { get_mode_symbol },
-                lualine_b = { branch },
-                lualine_c = { diagnostics },
-                lualine_y = {
-                    { search_count, type = "lua_expr" } },
-                lualine_z = { {
-                    breadcrumbs,
-                    color = {
-                        fg = "#EDDB8A", bg = "#4f596e"
-                    },
-                    always_visible = false
-                } },
-            }
-        })
-    end,
+            icons_enabled = true,
+            draw_empty = false,
+            icon = "",
+          }
+        },
+        lualine_z = { diagnostics },
+      },
+      winbar = {
+        lualine_a = { get_mode_symbol },
+        lualine_b = { branch },
+        lualine_c = { diagnostics },
+        lualine_y = {},
+        lualine_z = { {
+          breadcrumbs,
+          color = {
+            fg = "#EDDB8A", bg = "#4f596e"
+          },
+          always_visible = false
+        } },
+      }
+    })
+  end,
 }
 
 return M
