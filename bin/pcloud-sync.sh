b8507a0671fcb3e4dea9f45795aed7a473d905d7
aemonge <andres@aemonge.com>
2024-03-26 11:17:37 +0100

    feat(sync): Updated the main bin and sbin to sync.

---
+++ bin/pcloud-sync.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERNAME="aemonge"
+USER_HOME="/home/$USERNAME"
+MOUNT_POINT="$USER_HOME/cloud"
+
+# Define directories to sync
+declare -A CLOUD_DIRS=(
+    ["/os"]="$USER_HOME/os"
+    ["/settings"]="$USER_HOME/settings"
+    ["/media/images"]="$USER_HOME/images"
+    ["/documents/dev"]="$USER_HOME/documents"
+    ["/public"]="$USER_HOME/public"
+    ["/stash"]="$USER_HOME/stash"
+    ["/documents/studies/UNED"]="$USER_HOME/studies"
+)
+
+# Perform initial sync for each directory
+for REMOTE_DIR in "${!CLOUD_DIRS[@]}"; do
+    LOCAL_DIR=${CLOUD_DIRS[$REMOTE_DIR]}
+    echo "Syncing $REMOTE_DIR to $LOCAL_DIR"
+    rclone bisync pcloud:$REMOTE_DIR $LOCAL_DIR \
+        | rclone bisync -v --resync pcloud:$REMOTE_DIR $LOCAL_DIR \
+
+done
+
+# Wait for all background processes to finish
+wait
