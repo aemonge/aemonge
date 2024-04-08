e89417e0aeb0e8c0e16be76e13d786a2907d26e8
aemonge <andres@aemonge.com>
2024-04-08 17:43:53 +0200

    feat(arch): Update script is finally working for all

--- bin/pcloud-sync.sh
+++ bin/pcloud-sync.sh
@@ -14,14 +14,30 @@ declare -A CLOUD_DIRS=(
     ["/documents/studies/UNED"]="$USER_HOME/studies"
 )
 
-# Perform initial sync for each directory
+# Array to store PIDs of background processes
+PIDS=()
+
+# Trap function to handle cleanup
+trap 'cleanup' INT TERM EXIT
+
+cleanup() {
+    # Kill all background processes
+    for pid in "${PIDS[@]}"; do
+        kill "$pid" 2>/dev/null
+    done
+    wait 2>/dev/null
+    echo "Cleanup completed."
+}
+
+# Perform initial sync for each directory in parallel
 for REMOTE_DIR in "${!CLOUD_DIRS[@]}"; do
     LOCAL_DIR=${CLOUD_DIRS[$REMOTE_DIR]}
     echo "Syncing $REMOTE_DIR to $LOCAL_DIR"
-    rclone bisync --checksum "pcloud:$REMOTE_DIR" "$LOCAL_DIR" ||
-        rclone bisync -v --resync --checksum "pcloud:$REMOTE_DIR" "$LOCAL_DIR"
-
+    rclone bisync --resync --checksum "pcloud:$REMOTE_DIR" "$LOCAL_DIR" &
+    PIDS+=($!)
 done
 
 # Wait for all background processes to finish
 wait
+
+echo "Sync completed."
