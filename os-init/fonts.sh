0e3a1a1d3392a627904d30706d379ff0d9553d59
aemonge <andres@aemonge.com>
2024-07-08 11:18:28 +0200

    feat(bin): Added a CLI boxify command. Diselic friendly font.

--- os-init/fonts.sh
+++ os-init/fonts.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
 
+iosevka_term() {
+  local url="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/IosevkaTerm.zip"
+  echo "Downloading and installing IosevkaTerm font..."
+
+  local temp_dir="/tmp/iosevka"
+  mkdir "$temp_dir" || rm -rf $temp_dir && mkdir $temp_dir
+  cd $temp_dir || (echo "Error: Failed to create $temp_dir directory" && exit 1)
+  wget "$url"
+  unzip IosevkaTerm.zip
+
+  local fonts_dir="$HOME/.local/share/fonts/"
+  local old_fonts_dir="$HOME/.fonts/m/"
+  mkdir -p "$fonts_dir"
+  mkdir -p "$old_fonts_dir"
+
+  cp -r IosevkaTermNerdFont* "$old_fonts_dir"
+  cp -r IosevkaTermNerdFont* "$fonts_dir"
+
+  echo "IosevkaTerm font installed successfully."
+}
+
+zed_mono() {
+  local url="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/ZedMono.zip"
+  echo "Downloading and installing ZedMono font..."
+
+  local temp_dir="/tmp/zedmono"
+  mkdir "$temp_dir" || rm -rf $temp_dir && mkdir $temp_dir
+  cd $temp_dir || (echo "Error: Failed to create $temp_dir directory" && exit 1)
+  wget "$url"
+  unzip ZedMono.zip
+
+  local fonts_dir="$HOME/.local/share/fonts/"
+  local old_fonts_dir="$HOME/.fonts/m/"
+  mkdir -p "$fonts_dir"
+  mkdir -p "$old_fonts_dir"
+
+  cp -r ZedMonoNerdFontMono* "$old_fonts_dir"
+  cp -r ZedMonoNerdFontMono* "$fonts_dir"
+
+  echo "ZedMono font installed successfully."
+}
+
 meslo () {
   echo "Downloading and installing Meslo font..."
 
@@ -20,4 +62,8 @@ meslo () {
   echo "Meslo font installed successfully."
 }
 
-meslo
+
+# meslo
+# iosevka_term
+zed_mono
+# sudo pacman -S ttc-iosevka
