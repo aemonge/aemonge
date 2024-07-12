4effe9acb73f3ec6e3368a3e08c659c2ff9f5de9
aemonge <andres@aemonge.com>
2024-07-12 11:14:39 +0200

    fix(resume): Use docs folder.

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+mv docs/avatar.png .
 pdflatex resume.tex
 
 rm resume.aux
 rm resume.log
 rm resume.out
 
-mv resume.pdf www/python-tech-lead_andres-monge.pdf
-
+mv resume.pdf docs/python-tech-lead_andres-monge.pdf
+mv avatar.png docs/.
