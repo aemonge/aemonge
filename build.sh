d6122b693ff126340af5a292430bfa4ba3f1f3d7
aemonge <andres@aemonge.com>
2024-07-16 11:59:36 +0200

    feat(build): Build resume and README everytime

--- build.sh
+++ build.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
+# Move avatar to root directory
 mv docs/avatar.png .
+
+# Compile LaTeX to PDF
 pdflatex resume.tex
 
-rm resume.aux
-rm resume.log
-rm resume.out
+# Clean up auxiliary files
+rm resume.aux resume.log resume.out
 
+# Move PDF to docs folder
 mv resume.pdf docs/python-tech-lead_andres-monge.pdf
-mv avatar.png docs/.
+
+# Move avatar back to docs folder
+mv avatar.png docs/
+
+# Generate README.md from resume.tex
+sed -n '/\\begin{document}/,/\\end{document}/p' resume.tex |
+sed -e 's/\\section{\([^}]*\)}/## \1/g' \
+    -e 's/\\subsection{\([^}]*\)}/### \1/g' \
+    -e 's/\\begin{itemize}//g' \
+    -e 's/\\end{itemize}//g' \
+    -e 's/\\item/- /g' \
+    -e 's/\\textbf{\([^}]*\)}/**\1**/g' \
+    -e 's/\\emph{\([^}]*\)}/*\1*/g' \
+    -e 's/\\href{\([^}]*\)}{\([^}]*\)}/[\2](\1)/g' \
+    -e 's/\\\\//g' \
+    -e '/^\s*$/d' > README.md
+
+# Add title to README.md
+sed -i '1i# Andres Monge - Python Tech Lead Resume\n' README.md
