e90c114f5bb5e35821b4ecda3282c4c1bc9ca2a1
aemonge <andres@aemonge.com>
2025-01-16 18:38:46 +0100

    feat(resume): Javascriptless

--- docs/assets/index.js
+++ docs/assets/index.js
@@ -1,86 +1,24 @@
-// Function to show the resume
-function showResume() {
-	if (articlesLink && resumeLink) {
-		articlesLink.classList.remove("active");
-		resumeLink.classList.add("active");
-	}
-	document.getElementById("render").style.display = "block";
-	document.getElementById("articleWrapper").style.display = "none";
-	renderPDF(
-		"python-tech-lead_andres-monge.pdf",
-		document.querySelector("#render"),
-	);
-}
-
-// Function to show the articles
-function showArticles() {
-	if (articlesLink && resumeLink) {
-		articlesLink.classList.add("active");
-		resumeLink.classList.remove("active");
-	}
+// Function to show an article
+function showArticle(url) {
+	document.getElementById("articleWrapperId").style.display = "block";
 	document.getElementById("render").style.display = "none";
-	document.getElementById("articleWrapper").style.display = "block";
-	document.getElementById("articleWrapper").src = "articles/index.html";
+	document.getElementById("home-content").style.display = "none";
 }
 
-function renderPage(page) {
-	let viewport = page.getViewport({ scale: 0.5 });
-	const DPI = 72;
-	const PRINT_OUTPUT_SCALE = DPI / 72;
-	const scale = canvasContainer.clientWidth / viewport.width;
-	const canvas = document.createElement("canvas");
-
-	const ctx = canvas.getContext("2d");
-	viewport = page.getViewport({ scale });
-
-	canvas.width = Math.floor(viewport.width * PRINT_OUTPUT_SCALE);
-	canvas.height = Math.floor(viewport.height * PRINT_OUTPUT_SCALE);
-	canvas.style.width = "100%";
-
-	canvas.style.transform = "scale(1,1)";
-	canvas.style.transformOrigin = "0% 0%";
-
-	const canvasWrapper = document.createElement("div");
-
-	canvasWrapper.style.width = "100%";
-	canvasWrapper.style.height = "100%";
-
-	canvasWrapper.appendChild(canvas);
-
-	const renderContext = {
-		canvasContext: ctx,
-		viewport,
-	};
-
-	canvasContainer.appendChild(canvasWrapper);
-
-	page.render(renderContext);
-}
-
-function renderPages(pdfDoc) {
-	for (let num = 1; num <= pdfDoc.numPages; num += 1)
-		pdfDoc.getPage(num).then(renderPage);
-}
-
-// PDF rendering logic
-function renderPDF(url, canvasContainer) {
-	pdfjsLib.disableWorker = true;
-	pdfjsLib.getDocument(url).promise.then(renderPages, (error) => {
-		console.error("Error loading PDF:", error);
-	});
+// Function to show home content
+function showHome() {
+	document.getElementById("home-content").style.display = "block";
+	document.getElementById("articleWrapperId").style.display = "none";
+	document.getElementById("render").style.display = "none";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-	// Include the PDF.js library
-	const pdfjsLib = window["pdfjs-dist/build/pdf"];
-
 	const resumeLink = document.querySelector('a[href="#resume"]');
 	const articlesLink = document.querySelector('a[href="#articles"]');
 
 	// Check if the links are not null
 	if (resumeLink && articlesLink) {
 		// Add event listeners to the links
-		resumeLink.addEventListener("click", showResume);
 		articlesLink.addEventListener("click", showArticles);
 	}
 
@@ -91,6 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		link.target = "_blank";
 		link.href = "python-tech-lead_andres-monge.pdf";
 		link.click();
+		document.body.removeChild(link); // Clean up
 	});
 
 	showArticles();
